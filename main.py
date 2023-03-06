deb4e567e7b364085c864471d181782241160a1e
C. M. Leszczyński <cy.m.leszczynski@gmail.com>
2023-03-06 11:22:58 +0100

    nowy brancz

--- main.py
+++ main.py
@@ -7,4 +7,26 @@ import math
 from sklearn.preprocessing import MinMaxScaler, StandardScaler
 from sklearn.model_selection import train_test_split
 
-print("Testy!")
+# 1. Zaczytaj dane
+# 2. Sprawdź wymiary
+# 3. Usuń duplikaty
+# 4. Zmień typ danych
+# 5. Uzupełnij/ usuń braki
+# 6. Zmień dane kategoryczne na numeryczne
+# 7. Znormalizuj dane
+# 8. Podziel zbiór na uczący i testowy
+
+# Wczytywanie
+# 1. zaczytaj dane z pliku csv
+df = pd.read_csv("train.csv", sep=",", encoding='utf-8')
+# 2. sprawdź liczbę kolumn i
+print("Info:")
+df.info()
+# wyświetl część tabeli
+print("Nagłówek:")
+df.head()
+# 3. usuń wiersze z duplikatami id
+df.drop_duplicates(subset="ID", inplace=True)
+
+# 4.
+
